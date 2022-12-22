452bf2b90cd31e73ee48957a4fb76db63e4ca68e
ekincitalha <stlmstalha@gmail.com>
2022-12-22 11:47:52 +0300

    mutable example

---
+++ mutableandimmutable/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        example obj=new example("talha");
+        System.out.println(obj.getName());
+
+        obj.setName("talha1");
+        System.out.println(obj.getName());
+    }
+}
